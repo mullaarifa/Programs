3b467a753a64d8eb25e9c58d9bbc4ff225e81253
mullaarifa <78547037+mullaarifa@users.noreply.github.com>
2022-06-06 16:06:30 +0530

    Add files via upload

---
+++ ass17_1.c
@@ -0,0 +1,48 @@
+//Problem Statement
+//Accept Number From User and Display Below PAttern.
+//input row = 4 col = 4
+/*
+	*
+	*  *
+	*  *  *
+	*  *  *  *	
+*/
+
+#include<stdio.h>
+
+void Display(int iRow, int iColumn)
+{
+	int i = 0;
+	int j = 0;
+	
+	if(iRow != iColumn)
+	{
+		return;
+	}
+	
+	for(i = 1; i <= iRow; i++)
+	{
+		for(j = 1; j<= iColumn; j++)
+		{
+			if((i >= j)) 
+			{
+				printf("*\t");
+			}
+		}
+		printf("\n");
+		
+	}
+}
+
+int main()
+{
+	int iValue1 = 0, iValue2 = 0;
+	
+	printf("Please Enter The Rows:\n");
+	scanf("%d",&iValue1);
+
+	printf("Please Enter The Columns:\n");
+	scanf("%d",&iValue2);
+	
+	Display(iValue1, iValue2);
+}
